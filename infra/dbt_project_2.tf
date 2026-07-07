c57a051f18e9aa0f1c595f05836c203b6cac5800
jagadeesh-damarla <jagadeeswararao.d@thoughtworks.com>
2026-07-07 16:55:03 +0530

    infra update

--- infra/dbt_project_2.tf
+++ infra/dbt_project_2.tf
@@ -1,7 +1,8 @@
 locals {
   dbt_project_2 = {
     job_name                     = "dbt-test-2"
-    container_image              = "us-central1-docker.pkg.dev/new-map-project-1538399427267/dbt-images/dbt_image_2:latest"
+    # Bootstrap placeholder so Terraform can create the Cloud Run Job before dbt_image_2 exists; integration deploy later updates it to the real runtime image.
+    container_image              = "us-docker.pkg.dev/cloudrun/container/hello"
     workflow_name                = "dbt_test_2"
     runtime_service_account      = "github-actions-dbt@new-map-project-1538399427267.iam.gserviceaccount.com"
     log_bucket_name              = "dbt_logs_test_2026"
